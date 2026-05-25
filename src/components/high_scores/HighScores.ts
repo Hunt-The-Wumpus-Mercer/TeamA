34ef33324bd994c33b4a40a911b66853265cb91c
Hadrian Xavier Drumright-Frost <132313743+Mermieno1@users.noreply.github.com>
2026-05-25 16:39:06 +0000

    Implemented Highscores

---
+++ src/components/high_scores/HighScores.ts
@@ -0,0 +1,44 @@
+import type { IHighScores, IPerformance } from "./IHighScores";
+
+export class HighScore {
+    // encryption key for JSON file
+    KEY = '1a2b3c';
+    MAX_SCORES = 10;
+    //calculates score
+    public calculateScore(perf: IPerformance): number {
+        if (!perf.won) return 0;
+
+        const baseScore = 1000;
+        const penalty = perf.moves*10;
+        const bonus = perf.arrowsLeft;
+        return Math.max(0, baseScore - penalty + bonus);
+    }
+    // returns scores
+    public getScores(): IHighScores[] {
+        const scoresJSON = localStorage.getItem(this.KEY);
+        if (!scoresJSON) return [];
+
+        try {
+            return JSON.parse(scoresJSON) as IHighScores[];
+        } catch {
+            return [];
+        }
+    }
+    // adds scores
+    public addScore(playerName: string, perf: IPerformance): void {
+        const score = this.calculateScore(perf);
+        if (score<=0) return;
+        const Entry: IHighScores = {
+            playerName: playerName || 'User',
+            score: score,
+        }
+        const currentScores = this.getScores();
+        currentScores.push(Entry);
+
+        //sort scores
+        currentScores.sort((a, b) => b.score - a.score);
+        //makes top score to add maximum number of shown scores
+        const topScores = currentScores.slice(0, this.MAX_SCORES);
+        localStorage.setItem(this.KEY, JSON.stringify(topScores));
+    }
+}
--- src/components/high_scores/IHighScores.ts
+++ src/components/high_scores/IHighScores.ts
@@ -1,2 +1,9 @@
 export interface IHighScores {
+    playerName: string;
+    score: number;
+}
+export interface IPerformance {
+    won: boolean;
+    moves: number;
+    arrowsLeft: number;
 }
