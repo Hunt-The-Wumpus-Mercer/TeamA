3ed4491c4db40b8d35fad92374444bf36ff5dd4d
Hadrian Xavier Drumright-Frost <132313743+Mermieno1@users.noreply.github.com>
2026-05-26 23:27:36 +0000

    Fixed Sound file names and reprogrammed deleted Trivia files

--- src/components/trivia/ITrivia.ts
+++ src/components/trivia/ITrivia.ts
@@ -1,2 +1,13 @@
+export type QuestionType = "MCQ" | "FRQ"
+export interface Question {
+    id: number;
+    type: QuestionType;
+    prompt: string;
+    options?: string[]; //MCQ answer choices
+    answer: string; 
+}
 export interface ITrivia {
+  questions: Question[];
+  getNextQuestion(): Question;
+  validateAnswer(questionId: number, userAnswer: string): boolean;
 }
