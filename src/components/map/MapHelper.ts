256801600494d579d2ea5039146a4ff595c82e11
goozwack <164587724+goozwack@users.noreply.github.com>
2026-05-29 20:04:56 +0000

    finished getHazardsInPlayerRoom

--- src/components/map/MapHelper.ts
+++ src/components/map/MapHelper.ts
@@ -1,5 +1,6 @@
 import type { ICave } from "/workspaces/TeamA/src/components/cave/ICave.ts";
 import { RoomNum, type MapObjectType } from "./IMap";
+import { Map } from "./Map";
 
 export class MapHelper
 {
@@ -18,8 +19,29 @@ export class MapHelper
      * If wumpus is present, it appears first in the result.
      */
     public static getHazardsInPlayerRoom(): string[] {
-        const wumpusRoom = Map.getRoomLocation("wumpus" as MapObjectType);
-        return [];
+        const hazards: string[] = []
+        const roomNums: number[] = [
+            Map.getRoomLocation("wumpus" as MapObjectType),
+            Map.getRoomLocation("bat1" as MapObjectType),
+            Map.getRoomLocation("bat2" as MapObjectType),
+            Map.getRoomLocation("pit1" as MapObjectType),
+            Map.getRoomLocation("pit2" as MapObjectType)
+        ];
+        const hazardNames: string[] = [
+            "wumpus",
+            "bat1",
+            "bat2",
+            "pit1",
+            "pit2",
+        ];
+        const playerRoom: number =  Map.getRoomLocation("player" as MapObjectType)
+        for (const hazardRoom of roomNums) {
+            if (playerRoom == hazardRoom) {
+                hazards.push(hazardNames[roomNums.indexOf(room)])
+            }
+
+        }
+        return hazards;
     }
 
     /**
